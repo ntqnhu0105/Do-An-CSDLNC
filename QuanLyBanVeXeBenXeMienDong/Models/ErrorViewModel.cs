15cafaf92aa7b9e750b719989a5fa049408afe57
Quỳnh Như <ntqnhu.1504@gmail.com>
2025-03-23 20:02:29 +0700

    Delete QuanLyBanVeXeBenXeMienDong directory

--- QuanLyBanVeXeBenXeMienDong/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuanLyBanVeXeBenXeMienDong.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
